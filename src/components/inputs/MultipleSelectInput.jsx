7edf0931c51d1cb09c74b40b9ff86ccbf8848169
Desc <157510632+erwinGos@users.noreply.github.com>
2024-05-22 12:55:55 +0200

    formulaire ajout voiture

--- src/components/inputs/MultipleSelectInput.jsx
+++ src/components/inputs/MultipleSelectInput.jsx
@@ -3,16 +3,10 @@ import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 const people = [
-  { id: 1, name: 'Wade Cooper' },
-  { id: 2, name: 'Arlene Mccoy' },
-  { id: 3, name: 'Devon Webb' },
-  { id: 4, name: 'Tom Cook' },
-  { id: 5, name: 'Tanya Fox' },
-  { id: 6, name: 'Hellen Schmidt' },
-  { id: 7, name: 'Caroline Schultz' },
-  { id: 8, name: 'Mason Heaney' },
-  { id: 9, name: 'Claudie Smitham' },
-  { id: 10, name: 'Emil Schaefer' },
+  { id: 1, name: 'Climatisation' },
+  { id: 2, name: 'Chien autorisés' },
+  { id: 3, name: 'Sièges chauffants' },
+  { id: 4, name: 'Sièges massants' }
 ]
 
 function classNames(...classes) {
--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import RadioSelector from "../components/RadioSelector";
-import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
+import { ChevronDownIcon, ChevronUpIcon, ChevronRightIcon, PlusIcon } from '@heroicons/react/24/solid';
 import MultipleSelectInput from '../components/inputs/MultipleSelectInput';
 
 
 const RegisterForm = () => {
 
   const [selectedWrapper, setSelectedWrapper] = useState(0);
+  const [toggleForm, setToggleForm] = useState(false);
   const [addedCars, setAddedCars] = useState([]);
   const [mailingList, setMailingList] = useState([
     { 
@@ -35,6 +36,39 @@ const RegisterForm = () => {
       icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
             </svg>
+    }
+  ]);
+  // ATTENTION : à supprimer après l'ajout des données de l'api
+  const [userCars, setUserCars] = useState([
+    {
+      immatriculation: "BY-264-SS",
+      brand: "Citroën",
+      car_type: "Berline",
+      model: "C3 AirCross",
+      registration_date: "2022",
+      color: "Gray",
+      seats: 4,
+      options: ["Climatisation", "Chien Autorisé"]
+    },
+    {
+      immatriculation: "TD-041-DF",
+      brand: "Peugeot",
+      car_type: "Break",
+      model: "508 PSA",
+      registration_date: "2022",
+      color: "Gray",
+      seats: 4,
+      options: ["Climatisation", "Chien Autorisé"]
+    },
+    {
+      immatriculation: "NN-885-QS",
+      brand: "BMW",
+      car_type: "Berline",
+      model: "550i",
+      registration_date: "2022",
+      color: "Gray",
+      seats: 4,
+      options: ["Climatisation", "Chien Autorisé"]
     }
   ]);
 
@@ -145,9 +179,10 @@ const RegisterForm = () => {
   }
 
   const carInformations = () => {
+
     return (
     <div className='py-14 animate__animated animate__fadeIn'>
-      {addedCars.length <= 0 ? (
+      {toggleForm ? (
       <form onSubmit={(e) => e.preventDefault()}>
         <div>
           <h2 className="text-[24px] my-6 font-semibold leading-7 text-gray-900">Ajouter un véhicule</h2>
@@ -249,25 +284,191 @@ const RegisterForm = () => {
             </div>
           </div>
         </div>
+        <div onClick={() => setToggleForm(false)} className='flex justify-end mt-10'>
+          <button className="text-[14px] rounded-md bg-[#000000] px-6 py-2 w-[153px] h-[45px] text-sm text-center text-white shadow-sm hover:bg-[#141414] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition-all duration-200">
+            Enregistrer
+          </button>
+        </div>
+      </form>
+      ) 
+      : 
+      <fieldset>
+        <h2 className="text-[24px] my-6 font-semibold leading-7 text-gray-900">Ajouter un véhicule</h2>
+        <div className="space-y-5">
+          {userCars.map((car) => (
+            <div className="relative flex items-start justify-between p-4 border-[1px] rounded-md border-gray-300" >
+              <div className='flex'>
+                <div className="flex h-6 items-center">
+                  <input
+                    id={car.immatriculation}
+                    aria-describedby=""
+                    name="car"
+                    type="radio"
+                    defaultChecked=""
+                    className="h-4 w-4 border-gray-300 accent-black	 focus:ring-gray-200 cursor-pointer"
+                  />
+                </div>
+                <div className="ml-3 text-sm leading-6 flex flex-col">
+                  <label htmlFor="car_name" className="font-semibold text-[16px] text-gray-900">
+                    {car.brand} {car.model}
+                    <span id="description" className="font-semibold text-[16px] text-gray-500 m-2">
+                      {car.immatriculation}
+                    </span>
+                  </label>
+                  <span id="description" className="text-gray-500">
+                  {car.car_type}, {car.registration_date}, {car.seats} places disponibles hors conducteur
+                  </span>
+                </div>
+              </div>
+              <div className="ml-3 text-sm leading-6 flex flex-col hover:text-[#B19145] duration-200">
+                <button className='flex items-center'>
+                  <p className='mr-2 text-[16px]'>Modifier</p>
+                  <ChevronRightIcon className="w-5 h-5" />
+                </button>
+              </div>
+            </div>
+          ))}
+          <div onClick={() => setToggleForm(true)} className="relative flex items-start justify-between p-4 border-[1px] rounded-md border-gray-300 hover:bg-gray-100 cursor-pointer duration-200">
+              <div className='flex text-gray-600 justify-center items-center font-semibold'>
+                <PlusIcon className="w-6 h-6 m-2" />
+                Ajouter un véhicule
+              </div>
+          </div>
+        </div>
+      </fieldset>
+      }
+    </div>
+  );
+  }
+
+  const documentInformations = () => {
+    return (
+    <div className='py-14 animate__animated animate__fadeIn'>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <div className='grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4 my-4'>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Carte d'identité
+              </label>
+              <div className="relative rounded-md flex items-center">
+                <input
+                  type="file"
+                  name="first_name"
+                  id="first_name"
+                  className="block w-full outline-0	rounded-md border-0 py-3 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B19145] sm:text-sm sm:leading-6"
+                  placeholder="Prénom"
+                  autoComplete="off"
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Permis de conduire
+              </label>
+              <div className="relative rounded-md flex items-center">
+                <input
+                  type="file"
+                  name="first_name"
+                  id="first_name"
+                  className="block w-full outline-0	rounded-md border-0 py-3 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B19145] sm:text-sm sm:leading-6"
+                  placeholder="Prénom"
+                  autoComplete="off"
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                professionnelle de conducteur de taxi
+              </label>
+              <div className="relative rounded-md flex items-center">
+                <input
+                  type="file"
+                  name="first_name"
+                  id="first_name"
+                  className="block w-full outline-0	rounded-md border-0 py-3 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B19145] sm:text-sm sm:leading-6"
+                  placeholder="Prénom"
+                  autoComplete="off"
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4 my-4'>
+          <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Carte grise du véhicule
+              </label>
+              <div className="relative rounded-md flex items-center">
+                <input
+                  type="file"
+                  name="first_name"
+                  id="first_name"
+                  className="block w-full outline-0	rounded-md border-0 py-3 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B19145] sm:text-sm sm:leading-6"
+                  placeholder="Prénom"
+                  autoComplete="off"
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Extrait de KBIS
+              </label>
+              <div className="relative rounded-md flex items-center">
+                <input
+                  type="file"
+                  name="first_name"
+                  id="first_name"
+                  className="block w-full outline-0	rounded-md border-0 py-3 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#B19145] sm:text-sm sm:leading-6"
+                  placeholder="Prénom"
+                  autoComplete="off"
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className='flex justify-end mt-10'>
           <button className="text-[14px] rounded-md bg-[#000000] px-6 py-2 w-[153px] h-[45px] text-sm text-center text-white shadow-sm hover:bg-[#141414] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition-all duration-200">
             Enregistrer
           </button>
         </div>
       </form>
-      ) : null}
     </div>
   );
   }
 
-
   return (
-    <div className='min-h-[95vh] w-full md:px-[8rem] p-2'>
+    <div className='flex flex-col min-h-[95vh] w-auto md:px-[8rem] p-2'>
       <h2 className='mt-[100px] text-7xl text-center md:text-left font-medium m-1 animate__animated animate__fadeInDown'>{mailingList[selectedWrapper].title}</h2>
       <p className='my-6 font-medium text-[20px] text-center md:text-left animate__animated animate__fadeInDown'>{mailingList[selectedWrapper].description}</p>
       <RadioSelector setSelectedWrapper={setSelectedWrapper} mailingList={mailingList}/>
       {selectedWrapper == 0 ? personalInformations() : null}
       {selectedWrapper == 1 ? carInformations() : null}
+      {selectedWrapper == 2 ? documentInformations() : null}
     </div>
   )
 }
