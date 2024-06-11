f9a4082c3ec4cd56e9d67311d9f9f2afe65a2316
Desc <157510632+erwinGos@users.noreply.github.com>
2024-06-11 18:11:29 +0200

    update

--- src/App.js
+++ src/App.js
@@ -23,64 +23,66 @@ function App() {
   useEffect(() => {
     dispatch(checkTokenValidity());
     dispatch(GetCurrentInfos());
-    dispatch(GetRequiredDocuments()).then((RequiredDocuments) => {
-      dispatch(GetSentDocuments()).then((SentDocuments) => {
-        const requiredFiltered = RequiredDocuments.payload.filter(doc => doc != "CAR_REGISTRATION_CARD");
-        let sentDocs = [];
-        SentDocuments.payload.forEach(doc => sentDocs.push(doc.type));
-        let deepCopyMailingList = JSON.parse(JSON.stringify(mailingList));
-        let deepCopyTab = JSON.parse(JSON.stringify(mailingList[2]));
-        let tabIndex = deepCopyMailingList.findIndex(tab => tab.id == 3);
-        let copyTab = { ...deepCopyTab, completed: true };
-        deepCopyMailingList.splice(tabIndex, 1, copyTab);
-        dispatch(GetClientCars({page: 0, size :20})).then((res) => {
-          if(res.payload.length > 0) {
-            let CarCopyTab = JSON.parse(JSON.stringify(mailingList[1]));
-            let CarTabIndex = deepCopyMailingList.findIndex(tab => tab.id == 2);
-            let copyCarTab = { ...CarCopyTab, completed: true };
-            deepCopyMailingList.splice(CarTabIndex, 1, copyCarTab);
-          }
-        });
-        
-        dispatch(GetCurrentInfos()).then(res => {
-          if(res.payload.firstName != null ||
-            res.payload.lastName != null ||
-            res.payload.birthDate != null ||
-            res.payload.adress != null ||
-            res.payload.city != null ||
-            res.payload.zipCode != null ||
-            res.payload.siretNumber != null
-          ) {
-            if(res.payload.firstName.length >= 1 && 
-              res.payload.lastName.length >= 1 && 
-              res.payload.birthDate.length >= 1 && 
-              res.payload.adress.length >= 1 &&
-              res.payload.city.length >= 1 &&
-              res.payload.zipCode.length >= 1 &&
-              res.payload.siretNumber.length >= 1
-            ) {
-              let deepCopyTab = JSON.parse(JSON.stringify(mailingList[0]));
-              let tabIndex = deepCopyMailingList.findIndex(tab => tab.id == 1);
-              let copyTab = { ...deepCopyTab, completed: true };
-              console.log(deepCopyMailingList)
-              console.log(tabIndex)
-              console.log(copyTab)
-              deepCopyMailingList.splice(tabIndex, 1, copyTab);
-              dispatch(updateTab(deepCopyMailingList));
-            } else {
-              let deepCopyTab = JSON.parse(JSON.stringify(mailingList[0]));
-              let tabIndex = deepCopyMailingList.findIndex(tab => tab.id == 1);
-              let copyTab = { ...deepCopyTab, completed: false };
-              deepCopyMailingList.splice(tabIndex, 1, copyTab);
-              dispatch(updateTab(deepCopyMailingList));
-            }
-          }
-        });
-        if(JSON.stringify(sentDocs) == JSON.stringify(requiredFiltered)) {
+    if(auth.isAuth == true) {
+      dispatch(GetRequiredDocuments()).then((RequiredDocuments) => {
+        dispatch(GetSentDocuments()).then((SentDocuments) => {
+          const requiredFiltered = RequiredDocuments.payload.filter(doc => doc != "CAR_REGISTRATION_CARD");
+          let sentDocs = [];
+          SentDocuments.payload.forEach(doc => sentDocs.push(doc.type));
+          let deepCopyMailingList = JSON.parse(JSON.stringify(mailingList));
+          let deepCopyTab = JSON.parse(JSON.stringify(mailingList[2]));
+          let tabIndex = deepCopyMailingList.findIndex(tab => tab.id == 3);
+          let copyTab = { ...deepCopyTab, completed: true };
           deepCopyMailingList.splice(tabIndex, 1, copyTab);
-        }
+          dispatch(GetClientCars({page: 0, size :20})).then((res) => {
+            if(res.payload.length > 0) {
+              let CarCopyTab = JSON.parse(JSON.stringify(mailingList[1]));
+              let CarTabIndex = deepCopyMailingList.findIndex(tab => tab.id == 2);
+              let copyCarTab = { ...CarCopyTab, completed: true };
+              deepCopyMailingList.splice(CarTabIndex, 1, copyCarTab);
+            }
+          });
+          
+          dispatch(GetCurrentInfos()).then(res => {
+            if(res.payload.firstName != null ||
+              res.payload.lastName != null ||
+              res.payload.birthDate != null ||
+              res.payload.adress != null ||
+              res.payload.city != null ||
+              res.payload.zipCode != null ||
+              res.payload.siretNumber != null
+            ) {
+              if(res.payload.firstName.length >= 1 && 
+                res.payload.lastName.length >= 1 && 
+                res.payload.birthDate.length >= 1 && 
+                res.payload.adress.length >= 1 &&
+                res.payload.city.length >= 1 &&
+                res.payload.zipCode.length >= 1 &&
+                res.payload.siretNumber.length >= 1
+              ) {
+                let deepCopyTab = JSON.parse(JSON.stringify(mailingList[0]));
+                let tabIndex = deepCopyMailingList.findIndex(tab => tab.id == 1);
+                let copyTab = { ...deepCopyTab, completed: true };
+                console.log(deepCopyMailingList)
+                console.log(tabIndex)
+                console.log(copyTab)
+                deepCopyMailingList.splice(tabIndex, 1, copyTab);
+                dispatch(updateTab(deepCopyMailingList));
+              } else {
+                let deepCopyTab = JSON.parse(JSON.stringify(mailingList[0]));
+                let tabIndex = deepCopyMailingList.findIndex(tab => tab.id == 1);
+                let copyTab = { ...deepCopyTab, completed: false };
+                deepCopyMailingList.splice(tabIndex, 1, copyTab);
+                dispatch(updateTab(deepCopyMailingList));
+              }
+            }
+          });
+          if(JSON.stringify(sentDocs) == JSON.stringify(requiredFiltered)) {
+            deepCopyMailingList.splice(tabIndex, 1, copyTab);
+          }
+        });
       });
-    });
+    }
   },[])
 
   return (
